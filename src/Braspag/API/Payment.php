bb82eb0584263c9f039bbf6d3e569c871f80a316
William Costa <william@webart.com.br>
2019-12-18 17:50:35 -0300

    Add captured amount in populate method

--- src/Braspag/API/Payment.php
+++ src/Braspag/API/Payment.php
@@ -123,11 +123,13 @@ class Payment implements \JsonSerializable
         if (isset($data->CreditCard)) {
             $this->creditCard = new CreditCard();
             $this->creditCard->populate($data->CreditCard);
+            $this->setType(self::PAYMENTTYPE_CREDITCARD);
         }
 
         if (isset($data->DebitCard)) {
             $this->debitCard = new CreditCard();
             $this->debitCard->populate($data->DebitCard);
+            $this->setType(self::PAYMENTTYPE_DEBITCARD);
         }
 
         $this->expirationDate  =  isset($data->ExpirationDate)?$data->ExpirationDate: null;
@@ -146,6 +148,7 @@ class Payment implements \JsonSerializable
         $this->paymentId = isset($data->PaymentId)? $data->PaymentId: null;
         $this->type = isset($data->Type)? $data->Type: null;
         $this->amount = isset($data->Amount)? $data->Amount: null;
+        $this->capturedAmount = isset($data->CapturedAmount)? $data->CapturedAmount: null;
         $this->receivedDate = isset($data->ReceivedDate)? $data->ReceivedDate: null;
         $this->currency = isset($data->Currency)? $data->Currency: null;
         $this->country = isset($data->Country)? $data->Country: null;
@@ -156,6 +159,7 @@ class Payment implements \JsonSerializable
         $this->status = isset($data->Status)? $data->Status: null;
 
         $this->links = isset($data->Links)? $data->Links: [];
+        $this->extraDataCollection = isset($data->ExtraDataCollection)? $data->ExtraDataCollection: [];
     }
 
     public static function fromJson($json)
